9642f43fc9e41345f54b8a84cd713823f9770ce9
Nebyat Tibebu <nebyatweb@users.noreply.github.com>
2022-04-06 23:48:26 -0500

    add admin endpoint and authentication by user role functionality

---
+++ labs/lab5/src/main/java/edu/miu/controller/AdminController.java
@@ -0,0 +1,25 @@
+package edu.miu.controller;
+
+import edu.miu.domain.dto.UserDto;
+import edu.miu.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final UserService userService;
+
+    @GetMapping
+//    @PreAuthorize("hasRole('ADMIN')")
+    public List<UserDto> getUsers() {
+        return userService.findAll();
+    }
+}
